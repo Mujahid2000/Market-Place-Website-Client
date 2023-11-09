be0c044be1301a7382315f36b4cbcc2fb8e2bb13
Mujahid2000 <developermujahid2001@gmail.com>
2023-11-09 14:04:30 +0600

    update firebase file

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -18,9 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export default app;
 
-// apiKey: "AIzaSyDz38BC66y8MWtzS-b44nJamWwua69srEA",
-  // authDomain: "taskla-market-place.firebaseapp.com",
-  // projectId: "taskla-market-place",
-  // storageBucket: "taskla-market-place.appspot.com",
-  // messagingSenderId: "396562025600",
-  // appId: "1:396562025600:web:39a205de10f1aea762ea65"
